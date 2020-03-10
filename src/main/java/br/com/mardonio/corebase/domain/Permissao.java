99627b9daa58b54131425cd464e011855210932c
Mardonio Silva da Costa <mrdonio.sc@gmail.com>
2020-03-10 12:28:00 -0300

    Adicionando novas dependencias e criado um banco de dados h2 para criação automatica da base de dados

--- src/main/java/br/com/mardonio/corebase/domain/Permissao.java
+++ src/main/java/br/com/mardonio/corebase/domain/Permissao.java
@@ -2,9 +2,15 @@ package br.com.mardonio.corebase.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Permissao implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String descricao;
 	private String url;
