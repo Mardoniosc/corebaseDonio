ac8b46ffde4b068232fa157394fa3748876246f6
Mardonio Silva da Costa <mrdonio.sc@gmail.com>
2020-03-10 11:48:36 -0300

    Criado o projeto spring boot - maven

---
+++ src/main/java/br/com/mardonio/corebase/CoreBaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.mardonio.corebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mardonio/corebase/CoreBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mardonio.corebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
