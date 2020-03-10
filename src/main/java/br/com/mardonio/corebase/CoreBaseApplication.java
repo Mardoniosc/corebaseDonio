c5e869a8a4645ec38cf3a747d45bd9d182437bd1
Mardonio Silva da Costa <mrdonio.sc@gmail.com>
2020-03-10 14:36:06 -0300

    Criando operação de instanciação

--- src/main/java/br/com/mardonio/corebase/CoreBaseApplication.java
+++ src/main/java/br/com/mardonio/corebase/CoreBaseApplication.java
@@ -1,13 +1,32 @@
 package br.com.mardonio.corebase;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CoreBaseApplication {
+import br.com.mardonio.corebase.domain.Permissao;
+import br.com.mardonio.corebase.repositories.PermissaoRepository;
 
+@SpringBootApplication
+public class CoreBaseApplication implements CommandLineRunner{
+
+	@Autowired
+	private PermissaoRepository permissaoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CoreBaseApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		 Permissao p1 = new Permissao(null, "Home", "/home", null);
+		 Permissao p2 = new Permissao(null, "DashBoard", "/dashboard", null);
+		 Permissao p3 = new Permissao(null, "Usuario", "/usuario", null);
+		 
+		 permissaoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
--- src/main/java/br/com/mardonio/corebase/domain/Permissao.java
+++ src/main/java/br/com/mardonio/corebase/domain/Permissao.java
@@ -21,6 +21,7 @@ public class Permissao implements Serializable {
 	
 	@NotNull
 	private String descricao;
+	
 	@NotNull
 	private String url;
 	
